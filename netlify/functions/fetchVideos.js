4edb736708fd50762390556318bb63dc1304f042
lennyrajan <lennyrajan@gmail.com>
2025-06-27 10:48:31 -0700

    Update fetchVideos.js

--- netlify/functions/fetchVideos.js
+++ netlify/functions/fetchVideos.js
@@ -2,27 +2,18 @@
 
 const fetch = require('node-fetch');
 
+const API_KEY = process.env.YOUTUBE_API_KEY;
+const CHANNEL_ID = "UC9BQ_0nHNwEkW7YXdBcqljg";
+
+const getVideos = async () => {
+  const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=12`);
+  const data = await res.json();
+  return data.items.filter(item => item.id.kind === "youtube#video");
+};
+
 exports.handler = async function(event, context) {
-  const API_KEY = process.env.YOUTUBE_API_KEY;
-  const CHANNEL_USERNAME = "kidsstoriesandmagicalmoments";
-
-  const getChannelId = async () => {
-    const res = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=id&forUsername=${CHANNEL_USERNAME}&key=${API_KEY}`);
-    const data = await res.json();
-    return data.items?.[0]?.id;
-  };
-
-  const getVideos = async (channelId) => {
-    const res = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${API_KEY}&channelId=${channelId}&part=snippet,id&order=date&maxResults=12`);
-    const data = await res.json();
-    return data.items.filter(item => item.id.kind === "youtube#video");
-  };
-
   try {
-    const channelId = await getChannelId();
-    if (!channelId) throw new Error("Channel ID not found");
-
-    const videos = await getVideos(channelId);
+    const videos = await getVideos();
     return {
       statusCode: 200,
       body: JSON.stringify(videos)
